22c76f09bef39272087de8036ccbd838b7a48dff
milesvp <milesvp@83c5f06f-114d-0410-a33c-9ed400b1a7cd>
2010-01-01 00:08:24 +0000

    First draft of a Table(object) Class. Used to collect card images.
    git-svn-id: https://svn.picturecrossword.com/pokerbot/trunk@40 83c5f06f-114d-0410-a33c-9ed400b1a7cd

---
+++ screen_capture.py
@@ -0,0 +1,97 @@
+import pywinauto
+from pywinauto import findwindows
+from pywinauto.application import Application
+import PIL
+from time import sleep
+
+app = Application()
+fw = pywinauto.findwindows
+find_win = fw.find_window
+app.connect_(title_re = '.* NL Hold.em')
+win = app.window_(handle = find_win(title_re = '.*NL Hold.em'))
+
+class Table(object):
+  def __init__(self, table_name, game_type = "NL Hold'em"):
+    self.name = table_name
+    self.game_type = game_type
+    self.app = Application()
+
+    self.app.connect_(title_re = self.name + '.* ' + self.game_type)
+
+    self.table_handle = findwindows.find_window( title_re = self.name + '.* ' + self.game_type)
+    self.chat_parent_handle = self.GetChatParentHandle()
+    self.chat_handle = self.GetChatHandle()
+    self.chat_text = []
+    self.GetChatText()
+
+    self.lines_processed = len(self.chat_text)
+    self.table_cards = []
+
+  def GetChatParentHandle(self):
+    for handle in findwindows.find_windows(process = self.app.process):
+      texts = app.window_(handle = handle).GetProperties()['Texts'][0]
+      if (    ("UB" not in texts)
+          and (self.name in texts)
+          and ("NL" not in texts)):
+        return handle
+
+  def GetChatHandle(self):
+    parent_win = self.app.window_(handle = self.chat_parent_handle)
+    for child_win in parent_win.Children():
+      props = child_win.GetProperties()
+      if (    (props['IsVisible'])
+          and (props['Class'] == 'RichEdit20A')
+          and ('connected' in props['Texts'][0])):
+        return child_win.handle  
+
+  def GetChatText(self):
+    self.chat_text = app.window_(handle = self.chat_handle).GetProperties()['Texts'][0].split('\r\n')
+
+  def CaptureWindow(self, filename = 'd:/pokerbot/image.bmp'):
+    image = app.window_(handle = self.table_handle).CaptureAsImage()
+    image.save(filename)
+
+  def TimedCapture(self, total_images, timed_interval, filename = 'd:/pokerbot/screens/image.bmp'):
+    win = app.window_(handle = self.table_handle)
+    for i in xrange(total_images):
+      save_file = filename.rsplit('.',2)
+      save_file = save_file[0] + str(i) + '.' + save_file[1]
+      self.CaptureWindow(filename = save_file)
+      sleep(timed_interval)
+
+  def ProcessLines(self):
+    self.GetChatText()
+    while (self.lines_processed < len(self.chat_text)):
+      print self.lines_processed
+      self.ProcessLine(self.chat_text[self.lines_processed])
+      self.lines_processed += 1
+
+
+  def SetTableCards(self, line):
+    if ( ('FLOP' in line)
+       or('TURN' in line)
+       or('RIVER' in line)):
+      cards = line.rsplit('[',2)[1].rsplit(']',2)[0].split(' ')
+      for card in cards:
+        self.table_cards.append(card)
+      self.CaptureWindow('d:/pokerbot/screen3/' + ''.join(self.table_cards) + '.bmp')
+    elif 'HOLE CARDS' in line:
+      self.table_cards = []
+
+  def ProcessLine(self, line):
+    if 'DEALING' in line:
+      self.SetTableCards(line)
+    if 'SHOW DOWN' in line:
+      pass
+
+          
+
+def CardCapture(table_name, total_captures):
+    table_handle = GetTableHandle(table_name)
+    for i in xrange(total_captures):
+        text = GetChatText(table_name)
+
+
+#CaptureWindow(GetTableHandle('Tonga'))
+#print GetDealerChat('Tonga').split('\r\n',2)[0]
+print 'done'
